61bde0d12234b8840bc457444774e3b999e3eafc
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-07 11:29:37 -0400

    added heart icon instead of hambuger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
-          Natalia Carvajal <i class="far fa-heart"></i>
+          Natalia Carvajal
         </a>
         <button
           className="navbar-toggler"
@@ -16,28 +16,29 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          {/* <span className="navbar-toggler-icon"></span> */}
+          <i className="far fa-heart"></i>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">
-                About Me <i class="far fa-heart"></i>
+                About Me
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Skills <i class="far fa-heart"></i>
+                Skills
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Projects <i class="far fa-heart"></i>
+                Projects
               </a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Connect <i class="far fa-heart"></i>
+                Connect
               </a>
             </li>
           </ul>
