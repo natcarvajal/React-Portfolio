563dc03c6ff3be737a08d3476311774251517d25
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-07 14:14:37 -0400

    added about me portion

---
+++ src/components/AboutMe.js
@@ -0,0 +1,57 @@
+import React from "react";
+import baxter from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/baxter-cropped.png";
+
+function AboutMe() {
+  return (
+    <div className="about-me">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6 col-md-12">
+            <div className="baxter">
+              <img
+                id="imgHead"
+                src={baxter}
+                alt="my pug"
+                className="img-fluid"
+              ></img>
+            </div>
+            <div className="col-lg-6 col-md-12" id="about">
+              <h2 className="about-text pt-5">
+                <span>A LITTLE</span>
+                <span>ABOUT ME</span>
+              </h2>
+              <div className="paragraph py-4 w-75">
+                <p className="para">
+                  My name is Natalia Carvajal and I am an aspiring software
+                  developer. I am currenlty enrolled in Columbia's Engineering
+                  Coding Bootcamp. I received my Bachelor's Degree in
+                  Communications from the University of Charleston. After three
+                  years and several internhsips, I realized I was going down the
+                  wrong path and needed a change as soon as possible. I have
+                  always been interested in coding but never knew where to
+                  start, until this year. I have a lot to learn and I am excited
+                  to see what the future holds.{" "}
+                </p>
+                <p className="para">My hobbies include:</p>
+                <ul>
+                  <li className="hobb">
+                    Going to the gym <i className="fas fa-dumbbell"></i>
+                  </li>
+                  <li className="hobb">
+                    Drinking tons of coffee <i className="fas fa-mug-hot"></i>
+                  </li>
+                  <li className="hobb">
+                    Taking my dog, Baxter, on longs walks{" "}
+                    <i className="fas fa-paw"></i>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AboutMe;
