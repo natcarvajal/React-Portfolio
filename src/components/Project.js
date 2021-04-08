806acf481fdbc96433af698db3babea934f8f0d4
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-08 10:18:21 -0400

    added two more projects and linked them accordingly

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,8 @@ import burg from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/im
 import burger from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/burger.png";
 import pets from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/project.png";
 import weather from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/weather.png";
+import quiz from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/code.png";
+import note from "/Users/nataliacarvajal/Desktop/gitrepos/React-Portfolio/src/img/note-taker.png";
 function Project() {
   return (
     <div className="project">
@@ -29,14 +31,16 @@ function Project() {
                       <a
                         href="https://github.com/streamingTurtles/project2-HW"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        GITHUB REPO
+                        Github Repo
                       </a>
                       <a
                         href="https://radiant-oasis-56969.herokuapp.com/"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        HEROKU LINK
+                        Heroku Link{" "}
                       </a>
                     </div>
                   </div>
@@ -60,14 +64,16 @@ function Project() {
                       <a
                         href="https://github.com/streamingTurtles/iSeePets"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        GITHUB REPO
+                        Github Repo
                       </a>
                       <a
                         href="https://streamingturtles.github.io/iSeePets/index.html"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        DEPLOYED LINK
+                        Deployed Link
                       </a>
                     </div>
                   </div>
@@ -91,14 +97,16 @@ function Project() {
                       <a
                         href="https://github.com/natcarvajal/Burger-13"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        GITHUB REPO
+                        Github Repo
                       </a>
                       <a
                         href="https://fast-dawn-24088.herokuapp.com/"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        HEROKU LINK
+                        Heroku Link
                       </a>
                     </div>
                   </div>
@@ -117,19 +125,87 @@ function Project() {
                     ></img>
                     <div className="card-body">
                       <h5 id="pro-title" className="card-title">
-                        Weather App
+                        Weather Dashboard
                       </h5>
                       <a
                         href="https://github.com/natcarvajal/Weather-Dashboard-HW-6"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        GITHUB REPO
+                        Github Repo
                       </a>
                       <a
                         href="https://natcarvajal.github.io/Weather-Dashboard-HW-6/"
                         className="btn button primary-button"
+                        id="link-buttons"
                       >
-                        DEPLOYED LINK
+                        Deployed Link
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="myproject">
+                <div className="img">
+                  <div className="card">
+                    <img
+                      id="projectImg"
+                      src={note}
+                      alt="project-1"
+                      href="#"
+                    ></img>
+                    <div className="card-body">
+                      <h5 id="pro-title" className="card-title">
+                        Note Taker{" "}
+                      </h5>
+                      <a
+                        href="https://github.com/natcarvajal/Note-Taker-HW-11"
+                        className="btn button primary-button"
+                        id="link-buttons"
+                      >
+                        Github Repo
+                      </a>
+                      <a
+                        href="https://aqueous-castle-38143.herokuapp.com/"
+                        className="btn button primary-button"
+                        id="link-buttons"
+                      >
+                        Heroku Link
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="myproject">
+                <div className="img">
+                  <div className="card">
+                    <img
+                      id="projectImg"
+                      src={quiz}
+                      alt="project-1"
+                      href="#"
+                    ></img>
+                    <div className="card-body">
+                      <h5 id="pro-title" className="card-title">
+                        Code Quiz
+                      </h5>
+                      <a
+                        href="https://github.com/natcarvajal/Code-Quiz-HW-4"
+                        className="btn button primary-button"
+                        id="link-buttons"
+                      >
+                        Github Repo
+                      </a>
+                      <a
+                        href="https://natcarvajal.github.io/Code-Quiz-HW-4/"
+                        className="btn button primary-button"
+                        id="link-buttons"
+                      >
+                        Deployed Link
                       </a>
                     </div>
                   </div>
